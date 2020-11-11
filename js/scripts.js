0fc993d1e750f042b9b0ed3a2bb32bd31a85a894
jahongirrahmatullayev <rahmatullayevjahongir997@gmail.com>
2020-11-12 01:32:31 +0300

    I wrote js code for a movie theater ticket purchase project

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+var USD_IN_UZS = 10374;
+var betmenMovieTicketingPrice = 21;
+var elShreddingForm = document.querySelector('.money-shredding');
+var elInsertedInput = elShreddingForm.querySelector('.money-inserted');
+var elAnsverResult = elShreddingForm.querySelector('.result-cash');
+var elResultFailure = elShreddingForm.querySelector('.result-failure');
+var elResultView = document.querySelector('.result-view');
+var elUnsatisfactoryResult = document.querySelector('.unsatisfactory-result');
+var elUnsatisfactoryResultTop = document.querySelector('.unsatisfactory-result-top');
+var elResultForm = document.querySelector('.result-form');
+
+var elInsufficientPrice = elShreddingForm.querySelector('.insufficient-price');
+
+     
+elShreddingForm.addEventListener('submit', function(evt){
+         evt.preventDefault();
+         enteredValue = parseFloat(elInsertedInput.value, 10);
+         var currencyExchange = enteredValue / USD_IN_UZS ;
+         elResultForm.classList.remove('d-none');
+
+         if ( currencyExchange >= betmenMovieTicketingPrice){
+           elResultView.classList.remove('d-none');
+           elResultForm.classList.add('alert-success');
+           elAnsverResult.textContent = currencyExchange;
+           
+         } else {
+           var notEnoughMoney = betmenMovieTicketingPrice - currencyExchange;
+           elUnsatisfactoryResult.classList.remove('d-none');
+           elUnsatisfactoryResultTop.classList.remove('d-none');
+           elResultForm.classList.add('alert-danger');
+           elResultFailure.textContent = currencyExchange;
+           elInsufficientPrice.textContent = notEnoughMoney ;
+         }
+});
+  
